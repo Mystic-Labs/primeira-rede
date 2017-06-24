ad59e3c8a606bf6fbcb3513b78f736addf563ac0
artmario <artmario@gmail.com>
2017-06-24 15:32:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data #substituir por um open("entrada.csv",'r')
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 import tensorflow as tf
 sess = tf.InteractiveSession()
@@ -20,7 +20,7 @@ cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)#definicao de otimizador ,taxa de aprendizado
 for _ in range(1000):#quantidade de treinos
-  batch = mnist.train.next_batch(100) # lote de treino
+  batch = mnist.train.next_batch(100) # lote de treino/ prepara uma imagem em vetor e um vetor de 10 posições pra cada caso de treino
   train_step.run(feed_dict={x: batch[0], y_: batch[1]})#treino em si
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1)) #
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
